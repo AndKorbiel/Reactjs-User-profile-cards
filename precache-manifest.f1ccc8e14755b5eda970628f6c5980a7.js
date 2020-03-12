32021771e347ec479706048a895977db1292f857
Andrzej Korbiel <akorbiel@online.saba.com>
2020-03-12 16:10:48 +0100

    Updates

---
+++ precache-manifest.f1ccc8e14755b5eda970628f6c5980a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed847860764dcd205548ebce97e6fadd",
+    "url": "/Reactjs-User-profile-cards/index.html"
+  },
+  {
+    "revision": "1c1557c0ba13b028ef4c",
+    "url": "/Reactjs-User-profile-cards/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2227b50116fc5a58afd4",
+    "url": "/Reactjs-User-profile-cards/static/css/main.0323ec51.chunk.css"
+  },
+  {
+    "revision": "1c1557c0ba13b028ef4c",
+    "url": "/Reactjs-User-profile-cards/static/js/2.6b155d5b.chunk.js"
+  },
+  {
+    "revision": "2227b50116fc5a58afd4",
+    "url": "/Reactjs-User-profile-cards/static/js/main.5422d94c.chunk.js"
+  },
+  {
+    "revision": "0eb5064334f322fd1f38",
+    "url": "/Reactjs-User-profile-cards/static/js/runtime~main.e1748e5d.js"
+  }
+]);
